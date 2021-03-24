239d59f2fe27dab35b029c0ee76f896ed62b4ae1
Michael Thaler <michael.thaler@physik.tu-muenchen.de>
2021-03-24 07:46:05 +0100

    Use plasma color map.

--- random_walk.py
+++ random_walk.py
@@ -34,6 +34,6 @@ class RandomWalk:
             # Calculate the new position.
             x = self.x_values[-1] + x_step
             y = self.y_values[-1] + y_step
+
             self.x_values.append(x)
             self.y_values.append(y)
-
